95330a088424f029d4240562ca9a5d5d345c27eb
Nikolas Monteiro <nmonteiro1997@gmail.com>
2023-10-29 01:52:45 -0300

    Nikolas: Primeiro Commit

---
+++ ConsoleEstudos/ConsoleTestes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEstudos
+{
+    internal class ConsoleTestes
+    {
+        static void Main()
+        {
+            Console.WriteLine("Nikolas Teste");
+        }
+    }
+}
